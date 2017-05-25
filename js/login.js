59e07775b223359a5341cb5d5a0ceab19b46963b
MikeBowers <Mike.Bow@live.com>
2017-05-25 03:19:22 -0400

    test1

--- js/login.js
+++ js/login.js
@@ -1,33 +1,16 @@
-
-var items = ['admin','admin123',
-              'user','user123',
-              'guest','guest123'];
-//Fix the above array
-$('#btn_login').click(function(){
-  var usernameAttempt = $('#usernameField').val();
-  var passwordAttempt = $('#passwordField').val();
-  var usernameExists = false;
-  var usernameIndex;
-  var passwordMatchesUsername = false;
-  var failedAttempt = false;
-  for(var i = 0; i < items.length; i++)
-  {
-    if(items[i] == usernameAttempt){
-          usernameExists = true;
-          usernameIndex = i;
+function validate()
+{
+    if(   (document.getElementById("text1").value == "admin" && document.getElementById("text2").value == "admin123") ||
+          (document.getElementById("text1").value == "user" && document.getElementById("text2").value == "user123") ||
+          (document.getElementById("text1").value == "seaway" && document.getElementById("text2").value == "seaway123") ||
+          (document.getElementById("text1").value == "suuser" && document.getElementById("text2").value == "suuser123") )
+    {
+        alert( "Welcome!" );
+        location.href="index.html";
     }
-    else{
-      failedAttempt = true;
+    else
+    {
+        alert( "Login Failed." );
+        location.href="login.html";
     }
-  }
-
-  if(usernameExists == true)
-  {
-      if(items[usernameIndex + 1] == passwordAttempt){
-            passwordMatchesUsername = true;
-            alert("welcome!");
-      }
-  }
-
-
-});
+}
