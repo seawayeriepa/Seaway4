d7dbe5f26064b0b9c410692e194c7dfd07ec1745
MikeBowers <Mike.Bow@live.com>
2017-05-25 03:41:32 -0400

    test5

---
+++ js/login.js
@@ -0,0 +1,16 @@
+function validate()
+{
+    if(   (document.getElementById("text1").value == "admin" && document.getElementById("text2").value == "admin123") ||
+          (document.getElementById("text1").value == "user" && document.getElementById("text2").value == "user123") ||
+          (document.getElementById("text1").value == "seaway" && document.getElementById("text2").value == "seaway123") ||
+          (document.getElementById("text1").value == "suuser" && document.getElementById("text2").value == "suuser123") )
+    {
+        alert( "Welcome!" );
+        location.href="index.html";
+    }
+    else
+    {
+        alert( "Login Failed." );
+        location.href="login.html";
+    }
+}
